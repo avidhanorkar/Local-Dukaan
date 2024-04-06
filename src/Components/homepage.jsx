3215f5f29f9bd31476245af6ef87fcf7529954d3
Avishkar Dhanorkar <136093003+avidhanorkar@users.noreply.github.com>
2024-04-06 21:21:40 +0000

    Testing Only-but failed

--- src/Components/homepage.jsx
+++ src/Components/homepage.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import Navbar from './navbar.jsx'
-import Hero from './hero.jsx'
-import Feature from './features.jsx'
-import Category from './Category.jsx';
-import Advantages from './/advantages.jsx';
 import User from './user-page/user.jsx';
 
 function Homepage(){
