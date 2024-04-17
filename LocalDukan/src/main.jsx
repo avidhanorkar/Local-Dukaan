d9d9e8626a3ec063ad9dc0bfc3ac9f7e5788b221
Avishkar Dhanorkar <136093003+avidhanorkar@users.noreply.github.com>
2024-04-17 10:55:15 +0000

    Authentication Added

--- LocalDukan/src/main.jsx
+++ LocalDukan/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Layout from './Layout.jsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import CategoryPage from './Components/UserPage/CategorySelection.jsx'
-import ShopCategory from './Components/UserPage/Store/shop-category.jsx'
-
+import DataCards from './Components/UserPage/Store/shop-category.jsx'
+import { Auth0Provider } from '@auth0/auth0-react';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -22,14 +22,22 @@ const router = createBrowserRouter([
       },
       {
         path: 'ShopSelection',
-        element: <ShopCategory />
+        element: <DataCards />
       }
     ]
   }
 ])
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-   <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Auth0Provider
+    domain="dev-ognzzav3v80lh3vr.us.auth0.com"
+    clientId="UyETfPujLFsJpHmCLUSq7Z95GLrB33dl"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <RouterProvider router={router} />
+  </Auth0Provider>
+);
